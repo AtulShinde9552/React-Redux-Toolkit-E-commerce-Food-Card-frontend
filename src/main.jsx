6695123b4cb1842feaf153ed6efaccaecdc9a9b5
AtulShinde9552 <atul387659@gmail.com>
2024-03-26 18:53:57 +0530

    NA

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/app/store.js";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
